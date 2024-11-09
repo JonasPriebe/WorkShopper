5740fb9e86a1f96cd283f470261f1986d89edae9
JonasPriebe <priebe.j@yahoo.com>
2024-11-09 13:45:29 +0000

    Enable finding Mod_Ids in Workshop items

--- workShopper.sh
+++ workShopper.sh
@@ -2,11 +2,14 @@
 
 WORKSHOP_ID=$1
 
+echo "Starting Search for $WORKSHOP_ID"
+echo
+echo
 HEADERS="Content-Type: application/x-www-form-urlencoded"
 
 DATA="collectioncount=1&publishedfileids[0]=$WORKSHOP_ID"
 
-RESPONSE=$(curl -X POST \
+RESPONSE=$(curl -s -X POST \
   https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/ \
   -H "$HEADERS" \
   -d "$DATA")
@@ -18,7 +21,7 @@ IFS=$'\n' read -rd '' -a array <<< "$PUBLISHEDFILEIDS"
 ITEMCOUNT="itemcount=${#array[@]}"
 
 
-curl_command="curl --location 'https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/'"
+curl_command="curl -s --location 'https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/'"
 curl_command+=" -H \"$HEADERS\"
 "
 curl_command+=" --data-urlencode '${ITEMCOUNT}'"
@@ -28,6 +31,25 @@ for i in "${!array[@]}"; do
   curl_command+=" --data-urlencode 'publishedfileids[$i]=${array[$i]}'"
 done
 
-echo "$curl_command"
+
 RESPONSE=$(eval $curl_command)
-echo "$RESPONSE"
+DESCRIPTIONS=$(jq '.response.publishedfiledetails[].description' <<< "$RESPONSE")
+mapfile -t DESCRIPTIONS_ARRAY <<< "$DESCRIPTIONS"
+
+CURRENTWORKSHOPID=0
+RESULTS=""
+for line in "${DESCRIPTIONS_ARRAY[@]}"; do
+    matches=()
+    while IFS= read -r match; do
+        clean_match=$(echo "$match" | sed 's/^Mod ID: //')
+        matches+=("$clean_match;")
+    done < <(echo "$line" | grep -o "Mod ID: [[:alnum:]_-]*")
+    if [ ${#matches[@]} -gt 1 ]; then
+        echo "Multiple Mod IDs found for ${array[$CURRENTWORKSHOPID]}: ${matches[@]}"
+        ((CURRENTWORKSHOPID++))
+    fi
+     RESULTS+="${matches[@]}"
+done
+RESULTS=${RESULTS%?}
+echo
+echo "Workshop IDs for Collection $WORKSHOP_ID: {$RESULTS}"
